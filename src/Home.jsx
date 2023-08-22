e0beceead3f68c850f2a3df1a1f50d167e7db41f
SoftLife0 <onikosiadewale18@gmail.com>
2023-08-22 05:45:23 -0400

    mabr3

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import sms from './assets/sms.png'
 import { Link } from 'react-router-dom';
-import Dashboard from './Dashboard';
 
 
 
