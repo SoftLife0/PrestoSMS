b8c068a5fd1c74af4e47d073229721b8a11f8da9
SoftLife0 <onikosiadewale18@gmail.com>
2023-08-29 06:42:49 -0400

    adding sms page

--- src/pages/SendSms.jsx
+++ src/pages/SendSms.jsx
@@ -3,6 +3,7 @@ import '../css/Dashboard.css'
 import HeaderDashboard from '../components/HeaderDashboard';
 import Sidebar from '../components/Sidebar';
 import { useState } from 'react';
+import softlife from '../assets/softlife.png'
 
 function SendSms() {
     const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
@@ -11,6 +12,21 @@ function SendSms() {
         console.log("Triggered sidebar function")
         setOpenSidebarToggle(!openSidebarToggle)
     }
+
+    const [inputText, setInputText] = useState('');
+    const [savedText, setSavedText] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputText(event.target.value);
+    };
+
+    const handleSaveClick = () => {
+        setSavedText(inputText);
+    };
+
+    // Convert newline characters to <br> elements
+    const formattedText = savedText.replace(/\n/g, '<br>');
+
 
 
   return (
@@ -22,6 +38,31 @@ function SendSms() {
             <div className="main-title">
                 <h3>Import Number</h3>
             </div>
+
+            <div className="phone-view">
+                <div className="top-nav">
+                    <div className="profile-picture">
+                        <img src={softlife} alt=""width={100}/>
+                    </div>
+                </div>
+
+                <div className="message-container">
+                {inputText && (
+                    <p className="phone-text" dangerouslySetInnerHTML={{ __html: formattedText }} />
+                )}
+                </div>
+            </div>
+            <div className="input-container">
+                <textarea
+                rows="4"
+                cols="50"
+                value={inputText}
+                onChange={handleInputChange}
+                placeholder="Enter your SMS text here"/>
+            </div>
+
+            <button className='pill-button' onClick={handleSaveClick}>Save</button>
+
         </div>
 
     </main>
